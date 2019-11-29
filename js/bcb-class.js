292051fa2e390d0eee7dec3b6fd2f39fa9020331
brian <bcburnett@gmail.com>
2019-11-28 23:19:35 -0500

    added custom element lifecycle methods and comments

--- js/bcb-class.js
+++ js/bcb-class.js
@@ -4,19 +4,61 @@ import { connect } from 'pwa-helpers/connect-mixin.js';
 import { store } from './store.js';
 
 export class BcbClass extends connect(store)( LitElement) {
+
+  /**
+   * lit-element observed properties
+   */
   static get properties(){
     return {
       
     }
   }
+
+  /**
+   * lit-element styles array. external style sheets can be added here.
+   */
 static get styles() {
     return [css`
 
 `]
 }
+
+/**
+ * called when the component is created but before it is attached to the dom
+ */
   constructor(){
     super()
 
+  }
+
+  /**
+   * called after the first render, the shadow-dom is attached now.
+   */
+  firstUpdated(changedProperties){
+    console.log('first updated', changedProperties)
+  }
+
+  /**
+   *  Invoked when a component is added to the document’s DOM.
+   */
+  connectedCallback() {
+    super.connectedCallback()
+  
+    console.log('connected')
+  }
+
+  /**
+   *  Invoked when a component is removed from the document’s DOM.
+   */
+  disconnectedCallback() {
+    super.disconnectedCallback()
+  
+    console.log('disconnected')
+  }
+
+  attributeChangedCallback(e){
+    super.attributeChangedCallback();
+    console.log('attribute changed', e)
   }
 
   render(){
@@ -27,6 +69,10 @@ static get styles() {
 
 stateChanged(state) {
 console.log(state);
+}
+
+updated(changedProperties) {
+  console.log(changedProperties)
 }
 
 }
